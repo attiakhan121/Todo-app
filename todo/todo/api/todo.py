1c1be0c6f3d49bec6764dfea42b26cfc8f20efe3
attiakhan121 <attiakhantanveer@gmail.com>
2025-09-16 16:20:34 +0500

    add all fields

--- todo/todo/api/todo.py
+++ todo/todo/api/todo.py
@@ -1,9 +1,23 @@
-
-    # apps/todo/todo/api/todo.py
+# apps/todo/todo/api/todo.py
 import frappe
 
 @frappe.whitelist(allow_guest=True)
 def get_todo_list():
-    list = frappe.get_all("ToDo", fields=["name", "description", "status"])
-    
-    return list
+
+    todos = frappe.get_all(
+        "Todo item",   # Doctype ka exact name use karo (case sensitive)
+        fields=[
+            "name",        # Frappe ka internal ID
+            "title",       # Title field
+            "description", # Small Text field
+            "status",      # Select field
+            "due_date",    # Date field
+            "id",          # Data field
+            "text",        # Small Text field
+            "done",        # Checkbox field
+            "owner",       # System field (kis user ne create kiya)
+            "creation",    # System field (kab create hua)
+            "modified"     # System field (last update)
+        ]
+    )
+    return todos
